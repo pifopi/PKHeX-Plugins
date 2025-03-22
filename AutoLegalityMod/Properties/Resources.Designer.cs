28de8b2ccce4dbcf0d0e6e6d428a71eb2ac9be64
Lusamine <30205550+Lusamine@users.noreply.github.com>
2025-03-22 17:45:24 -0500

    Swap uploadgpss.png for a more consistent arrow

--- AutoLegalityMod/Properties/Resources.Designer.cs
+++ AutoLegalityMod/Properties/Resources.Designer.cs
@@ -392,9 +392,9 @@ namespace AutoModPlugins.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap uploadgpss {
+        internal static System.Drawing.Bitmap upload {
             get {
-                object obj = ResourceManager.GetObject("uploadgpss", resourceCulture);
+                object obj = ResourceManager.GetObject("upload", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
